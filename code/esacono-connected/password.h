f5bc5ac532d158917407aba4ab76f94ac64f5a0c
almostserena <serenacangiano@gmail.com>
2015-03-15 16:26:12 +0100

    Create password.h

---
+++ code/esacono-connected/password.h
@@ -0,0 +1,5 @@
+const String yunName="chooseaname";
+const String yunPsw="arduino";
+const String wifissid="wifiname";
+const String wifipsw="wifipassword";
+const String wifiAPname="my_Arduino_Yun"; //this is the name of the access point you want the yun to create if somethings goes wrong with the connection to the wifi
